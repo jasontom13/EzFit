c051f63c073dcc836788c4c535ed6fb6feb44cd7
Jason Tom <jtom77@gmail.com>
2015-03-07 00:51:25 -0700

    Finished goal activity

--- gen/edu/arizona/ezfit/activites/R.java
+++ gen/edu/arizona/ezfit/activites/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package edu.arizona.ezfit;
+package edu.arizona.ezfit.activites;
 
 public final class R {
     public static final class attr {
--- src/activities/MainActivity.java
+++ src/activities/MainActivity.java
@@ -3,11 +3,13 @@ package activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import bicepbuddy.Profile;
 import edu.arizona.ezfit.R;
 import extras.VerticalSeekBar;
 
@@ -16,13 +18,16 @@ public class MainActivity extends Activity{
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		VerticalSeekBar bar = (VerticalSeekBar) findViewById(R.id.goalSeekBar);
+		final VerticalSeekBar bar = (VerticalSeekBar) findViewById(R.id.goalSeekBar);
 		Button doneButton = (Button) findViewById(R.id.goalButton);
+		final Profile user = new Profile();
 		doneButton.setOnClickListener(new OnClickListener(){
-
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(MainActivity.this,DifficultyActivity.class);
+				user.setDifficulty(bar.getProgress());
+				Log.i("JASON", bar.getProgress()+"");
+				startActivity(i);
 				
 			}
 			

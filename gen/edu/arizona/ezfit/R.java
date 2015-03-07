30fef896e993aa751cb249366e3b0d7f449c8414
tfasulo <tfasulo@email.arizona.edu>
2015-03-07 01:31:22 -0700

    diifff

--- gen/edu/arizona/ezfit/R.java
+++ gen/edu/arizona/ezfit/R.java
@@ -14,23 +14,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060003;
-        public static final int button2=0x7f060001;
-        public static final int button3=0x7f060002;
+        public static final int diffLabel=0x7f060000;
         public static final int diffNext=0x7f060004;
-        public static final int editText1=0x7f06000a;
-        public static final int editText2=0x7f060006;
-        public static final int editText3=0x7f060008;
-        public static final int editText4=0x7f06000c;
-        public static final int goal=0x7f06000d;
-        public static final int goalButton=0x7f060010;
-        public static final int goalSeekBar=0x7f06000e;
-        public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060005;
-        public static final int textView3=0x7f060007;
-        public static final int textView4=0x7f060009;
-        public static final int textView5=0x7f06000b;
-        public static final int weightloss=0x7f06000f;
+        public static final int easyDiff=0x7f060001;
+        public static final int editText1=0x7f06000b;
+        public static final int editText2=0x7f060007;
+        public static final int editText3=0x7f060009;
+        public static final int editText4=0x7f06000d;
+        public static final int goal=0x7f06000e;
+        public static final int goalButton=0x7f060012;
+        public static final int goalSeekBar=0x7f06000f;
+        public static final int hardDiff=0x7f060003;
+        public static final int massGain=0x7f060011;
+        public static final int mediumDiff=0x7f060002;
+        public static final int textView1=0x7f060005;
+        public static final int textView2=0x7f060006;
+        public static final int textView3=0x7f060008;
+        public static final int textView4=0x7f06000a;
+        public static final int textView5=0x7f06000c;
+        public static final int weightloss=0x7f060010;
     }
     public static final class layout {
         public static final int activity_difficulty=0x7f030000;

552695ad43ac2ad989d07851371ed4cb6ef72a16
tfasulo <tfasulo@email.arizona.edu>
2015-03-06 22:36:44 -0700

    information activity

--- gen/edu/arizona/ezfit/R.java
+++ gen/edu/arizona/ezfit/R.java
@@ -17,11 +17,20 @@ public final class R {
         public static final int button1=0x7f060003;
         public static final int button2=0x7f060001;
         public static final int button3=0x7f060002;
+        public static final int editText1=0x7f060009;
+        public static final int editText2=0x7f060005;
+        public static final int editText3=0x7f060007;
+        public static final int editText4=0x7f06000b;
         public static final int textView1=0x7f060000;
+        public static final int textView2=0x7f060004;
+        public static final int textView3=0x7f060006;
+        public static final int textView4=0x7f060008;
+        public static final int textView5=0x7f06000a;
     }
     public static final class layout {
         public static final int activity_difficulty=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_information=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;

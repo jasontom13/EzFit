bbb36c1cdb5f4f432f456c7c96b190a9bce65870
Jason Tom <jtom77@gmail.com>
2015-03-08 02:25:37 -0700

    o

--- gen/edu/arizona/ezfit/R.java
+++ gen/edu/arizona/ezfit/R.java
@@ -19,19 +19,19 @@ public final class R {
     public static final class id {
         public static final int ageInfo=0x7f060006;
         public static final int ageText=0x7f060007;
-        public static final int checkmark=0x7f06001c;
-        public static final int childTxt=0x7f060019;
+        public static final int checkmark=0x7f06001b;
+        public static final int childTxt=0x7f060018;
         public static final int daysInfo=0x7f060010;
         public static final int diffLabel=0x7f060000;
         public static final int diffNext=0x7f060004;
         public static final int easyDiff=0x7f060001;
-        public static final int exerciseName=0x7f06001b;
+        public static final int exerciseName=0x7f06001a;
         public static final int ftInfo=0x7f06000d;
         public static final int ftText=0x7f06000c;
         public static final int goal=0x7f060013;
         public static final int hardDiff=0x7f060003;
         public static final int heightInfo=0x7f06000b;
-        public static final int imgDone=0x7f060018;
+        public static final int imgDone=0x7f060017;
         public static final int inInfo=0x7f06000f;
         public static final int inText=0x7f06000e;
         public static final int infoLabel=0x7f060005;
@@ -40,16 +40,15 @@ public final class R {
         public static final int mediumDiff=0x7f060002;
         public static final int nextInfo=0x7f060012;
         public static final int numDays=0x7f060011;
-        public static final int relativeLayout1=0x7f06001d;
-        public static final int rowTextView=0x7f06001f;
-        public static final int rowTextView2=0x7f060020;
-        public static final int star=0x7f06001e;
-        public static final int textView1=0x7f060016;
-        public static final int txtHeader=0x7f06001a;
+        public static final int relativeLayout1=0x7f06001c;
+        public static final int rowTextView=0x7f06001e;
+        public static final int rowTextView2=0x7f06001f;
+        public static final int star=0x7f06001d;
+        public static final int txtHeader=0x7f060019;
         public static final int weightInfo=0x7f060008;
         public static final int weightLoss=0x7f060014;
         public static final int weightText=0x7f060009;
-        public static final int workoutList=0x7f060017;
+        public static final int workoutList=0x7f060016;
     }
     public static final class layout {
         public static final int activity_difficulty=0x7f030000;
